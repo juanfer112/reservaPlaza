9a2c0bc0add57857687928769670c1c9ddb3edc3
Juan Pollan <juanferformacionyproyectos@gmail.com>
2020-06-19 15:23:40 +0000

    inserto de columnas horas en scheduler

--- src/js/component/scheduler.js
+++ src/js/component/scheduler.js
@@ -6,24 +6,30 @@ import "../../styles/home.scss";
 export const Scheduler = () => {
 	const days = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"];
 	var array = [];
-	for (let hours = 0; hours < 24; hours++) {
+	for (let hours = 0; hours < 25; hours++) {
+		let header = "HORAS";
 		let time = "";
 		let time2 = "";
-		if (hours < 10) {
-			time = `0${hours}:00`;
+
+		if (hours == 0) {
+			array.push(<div className="title text-center font-weight-bold">{header}</div>);
 		} else {
-			time = `${hours}:00`;
+			if (hours > 0 && hours < 11) {
+				time = `0${hours - 1}:00`;
+			} else {
+				time = `${hours - 1}:00`;
+			}
+			if (hours < 10) {
+				time2 = `0${hours}:00`;
+			} else {
+				time2 = `${hours}:00`;
+			}
+			array.push(<div className="cell text-center">{[time + "-" + (time2 !== "24:00" ? time2 : "00:00")]} </div>);
 		}
-		if (hours + 1 < 10) {
-			time2 = `0${hours + 1}:00`;
-		} else {
-			time2 = `${hours + 1}:00`;
-		}
-		array.push(<div className="cell">{[time + "-" + (time2 !== "24:00" ? time2 : "00:00")]} </div>);
 	}
 	return (
 		<div className="row justify-content-center">
-			<div className="col">{array}</div>
+			<div className="day col-1 p-0">{array}</div>
 			{days.map((item, index) => {
 				return <NewDay key={index} day={item} />;
 			})}
