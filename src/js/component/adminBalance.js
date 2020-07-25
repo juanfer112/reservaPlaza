00b19319d9c267aea20274ab96e24d5f9416a4db
Tozzigh <tozzigh@gmail.com>
2020-07-25 14:28:26 +0000

    Clean variables

--- src/js/component/adminBalance.js
+++ src/js/component/adminBalance.js
@@ -7,30 +7,30 @@ import { format, startOfDay, addHours, subHours } from "date-fns";
 
 export const AdminBalance = () => {
 	const { actions, store } = useContext(Context);
-	var holderAdminScheduler = [];
-	var day = startOfDay(new Date());
+	var adminScheduler = [];
+	var currentDay = startOfDay(new Date());
 
 	for (let hour = 0; hour < 25; hour++) {
 		var holderSpacesHours = [];
-		var thHours = hour < 11 ? `0${hour - 1}:00` : `${hour - 1}:00`;
+		var titleHour = hour < 11 ? `0${hour - 1}:00` : `${hour - 1}:00`;
 
-		for (let space = 0; space < store.spaces.length; space++) {
-			let spaceID = store.spaces[space]["id"];
-			let id = format(subHours(day, 1), "yyyy-MM-dd HH:mm:ss").toString();
+		for (let currentSpace = 0; currentSpace < store.spaces.length; currentSpace++) {
+			let spaceID = store.spaces[currentSpace]["id"];
+			let id = format(subHours(currentDay, 1), "yyyy-MM-dd HH:mm:ss").toString();
 			let className = actions.reservedDate(id, spaceID);
-			if (hour == 0 && space == 0) {
+			if (hour == 0 && currentSpace == 0) {
 				holderSpacesHours.push(
 					<>
 						<th />
-						<th>{store.spaces[space]["name"]}</th>
+						<th>{store.spaces[currentSpace]["name"]}</th>
 					</>
 				);
 			} else if (hour == 0) {
-				holderSpacesHours.push(<th>{store.spaces[space]["name"]}</th>);
-			} else if (space == 0) {
+				holderSpacesHours.push(<th>{store.spaces[currentSpace]["name"]}</th>);
+			} else if (currentSpace == 0) {
 				holderSpacesHours.push(
 					<>
-						<th>{thHours}</th>
+						<th>{titleHour}</th>
 						<td className={className} id={id}>
 							{id}
 						</td>
@@ -40,12 +40,12 @@ export const AdminBalance = () => {
 				holderSpacesHours.push(<td className={className} id={id} />);
 			}
 		}
-		holderAdminScheduler.push(<tr>{holderSpacesHours}</tr>);
-		day = addHours(day, 1);
+		adminScheduler.push(<tr>{holderSpacesHours}</tr>);
+		currentDay = addHours(currentDay, 1);
 	}
 	return (
 		<div className="container mt-5">
-			<table className="table table-bordered">{holderAdminScheduler}</table>
+			<table className="table table-bordered">{adminScheduler}</table>
 		</div>
 	);
 };
