bd8279dfc9757a0d846ed19b3d0161a65b04c55e
Juan Pollan <juanferformacionyproyectos@gmail.com>
2020-07-31 17:57:26 +0000

    ordenar key a renderizar en ficha cliente

--- src/js/component/profile.js
+++ src/js/component/profile.js
@@ -6,18 +6,14 @@ import "../../styles/home.scss";
 
 export const Profile = () => {
 	const { store, actions } = useContext(Context);
+	const sortStringKeys = (a, b) => (a[4] < b[0] ? 1 : -1);
 	var newArray = [];
 
 	var obj1 = Object.keys(store.user).map((element, index) => {
-		console.log(element);
 		if (element == "brands") {
 			let array = [];
-
 			Object.values(store.user[element]).map((brand, index) => {
-				console.log(brand);
-				console.log(brand["name"]);
 				newArray += array.concat(brand["name"], " ");
-				console.log(newArray);
 			});
 			return null;
 		}
@@ -32,54 +28,36 @@ export const Profile = () => {
 	return (
 		<div className="container">
 			<div className="row justify-content">
+				<img
+					src="https://previews.123rf.com/images/panyamail/panyamail1809/panyamail180900343/109879063-user-avatar-icon-sign-profile-symbol.jpg"
+					className="avatarProfile float-left rounded-circle mr-2 shadow"
+				/>
+
 				<div className="col-sm-10 col-md-6 shadow">
-					<img
-						src="https://previews.123rf.com/images/panyamail/panyamail1809/panyamail180900343/109879063-user-avatar-icon-sign-profile-symbol.jpg"
-						className="avatarProfile float-left rounded-circle mr-2"
-					/>
-
 					<div>
-						{Object.keys(store.user).map((element, index) => {
-							console.log(element);
-							console.log(store.user[element], "store.user[element]");
-
-							if (
-								element == "email" ||
-								element == "phone" ||
-								element == "name" ||
-								element == "tot_hours" ||
-								element == "cif"
-							) {
-								return (
-									<div key={index}>
-										{element}:{store.user[element]}
-									</div>
-								);
-							} else if (element == "brands") {
-								return (
-									<div key={index}>
-										{element}:{newArray}
-									</div>
-								);
-							}
-						})}
-					</div>
-
-					<div className="row justify-content">
-						<div className="col-sm-12 col-md-6">
-							<strong>email@gmail.com</strong>
-						</div>
-						<div className="col-sm-12 col-md-6">
-							<strong>CIF</strong>
-						</div>
-					</div>
-					<div className="row">
-						<div className="col-sm-12 col-md-6">
-							<strong>WEB</strong>
-						</div>
-						<div className="col-sm-12 col-md-6">
-							<strong>ADDRES</strong>
-						</div>
+						{Object.keys(store.user)
+							.sort(sortStringKeys)
+							.map((element, index) => {
+								if (
+									element == "email" ||
+									element == "phone" ||
+									element == "name" ||
+									element == "tot_hours" ||
+									element == "cif"
+								) {
+									return (
+										<div key={index}>
+											{element}:{store.user[element]}
+										</div>
+									);
+								} else if (element == "brands") {
+									return (
+										<div key={index}>
+											{element}:{newArray}
+										</div>
+									);
+								}
+							})}
 					</div>
 				</div>
 			</div>
