4a3012014d57bc5494fd179f7c3fedd7333a1aec
Tozzigh <tozzigh@gmail.com>
2020-08-29 17:17:39 +0000

    Need to check if token expire

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -86,7 +86,6 @@ const getState = ({ getStore, getActions, setStore }) => {
 				}
 				setStore({ token: null });
 				sessionStorage.clear();
-				window.location.reload();
 			},
 
 			pullEnterprises: async () => {
