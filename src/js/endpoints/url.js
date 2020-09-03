c292bc8c146bb99087552bc4a913dd22cf927cc8
Juan Pollan <juanferformacionyproyectos@gmail.com>
2020-09-03 11:35:14 +0000

    modificado url

--- src/js/endpoints/url.js
+++ src/js/endpoints/url.js
@@ -1,2 +1,2 @@
-const url = "https://3000-fdf3b7e1-cfb0-4b1a-b906-c0f1e00814a0.ws-eu01.gitpod.io/";
+const url = "https://3000-a2272d71-11c2-4a78-8b4c-24e04821553b.ws-eu01.gitpod.io/";
 export default url;
